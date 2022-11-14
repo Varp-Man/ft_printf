72baa1ecdad6330ec18df77aa775d03891cd4e03
Bohdan Korolov <bkorolov@c1r1p7.42vienna.com>
2022-11-14 15:04:36 +0100

    working . . .

---
+++ src/ft_printf.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkorolov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 13:15:49 by bkorolov          #+#    #+#             */
+/*   Updated: 2022/11/14 13:16:00 by bkorolov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_formats(va_list args, const char format);
+int		ft_putchar(int c);
+int		ft_printstr(char *str);
+int		ft_print_ptr(unsigned long long ptr);
+int		ft_printnbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int num, const char format);
+int		ft_printpercent(void);
+
+void	ft_putstr(char *str);
+void	ft_put_ptr(intptr_t num);
+int		ft_ptr_len(intptr_t num);
+char	*ft_itoa(unsigned int n);
+int		ft_num_len(unsigned	int num);
+void	ft_put_hex(unsigned int num, const char format);
+int		ft_hex_len(unsigned	int num);
+
+#endif
